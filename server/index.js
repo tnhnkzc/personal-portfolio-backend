4783df84bd4a0937dc7a35a8762b6b4fa69cda21
Tunahan <tunahankuzucuu@gmail.com>
2022-06-29 15:22:57 +0200

    Add the first route, install necessary dependencies

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import pageRoutes from "./routes/pages.js";
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/", pageRoutes);
+
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${5000}`))
+  )
+  .catch((error) => console.log(error.message));
---
+++ server/routes/pages.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { homePage } from "../controllers/pages.js";
+const router = express.Router();
+
+router.get("/", homePage);
+
+export default router;
