9af8aea890312227af164a07a79116446ea475ca
Tunahan <tunahankuzucuu@gmail.com>
2022-08-09 19:11:42 +0200

    Changes in index.js

--- index.js
+++ index.js
@@ -18,13 +18,6 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use("/", pageRoutes);
 app.use("/auth", userRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join("client", "build")));
-  app.get("*", (req, res) => {
-    req.sendFile(path.resolve("client", "build", "index.html"));
-  });
-}
-
 const url = process.env.MONGODB;
 
 mongoose
