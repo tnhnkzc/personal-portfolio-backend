2a02cd4603df4d79b79d9b255c6568a334e78bef
Tunahan <tunahankuzucuu@gmail.com>
2022-08-09 15:46:31 +0200

    changes in client index.js file

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use("/", pageRoutes);
 app.use("/auth", userRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static(path.join("./client/build")));
   app.get("*", (req, res) => {
     req.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
   });
