7ec1dd6d6aac0ac6bbe7500e18cef65fbca97c52
Tunahan <tunahankuzucuu@gmail.com>
2022-08-09 15:30:31 +0200

    changes in api file

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "https://tunahankuzucu.herokuapp.com/" });
 
 export const fetchProjects = () => API.get("/projects");
 
