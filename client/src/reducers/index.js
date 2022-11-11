f8207f6e6177b00e93c97747144b8d704ed0d5fc
tnhnkzc <tunahankuzucuu@gmail.com>
2022-11-11 20:15:07 +0100

    Delete the front-end folder

--- client/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import projects from "./projects";
-import auth from "./auth";
-
-export default combineReducers({ projects, auth });
--- client/src/styles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-export default makeStyles((theme) => ({
-  homeContainer: {
-    [theme.breakpoints.down("xs")]: {
-      position: "absolute",
-    },
-  },
-}));
